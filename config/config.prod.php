d7a01d418bada3138e3e33b13dd845f88fa5fdf7
D4rkf13nd <166979879+D4rkf13nd@users.noreply.github.com>
2025-08-20 11:46:01 +0800

    Delete config directory

--- config/config.prod.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST', 'your_production_db_host');
-define('DB_USER', 'your_production_db_user');
-define('DB_PASS', 'your_production_db_password');
-define('DB_NAME', 'dashboard_db');
-
-// Error reporting for production
-error_reporting(0);
-ini_set('display_errors', 0);
